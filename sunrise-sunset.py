856cab8a3d9bd0a52a5d0fb8fb622680c5c737f2
Mykola Bodnar <mykolab@nayax.com>
2022-01-27 12:45:33 +0200

    Added conn-server

--- sunrise-sunset.py
+++ sunrise-sunset.py
@@ -86,8 +86,10 @@ def openCurtains(scheduler):
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
-    scheduler.add_job(closeCurtains, 'date', run_date=sunTime("sunset",date.today() + timedelta(days = 1)), args=[scheduler] )
-    scheduler.add_job(openCurtains, 'date', run_date=sunTime("sunrise",date.today() + timedelta(days = 1, minutes = 30 )), args=[scheduler] )
+    # scheduler.add_job(closeCurtains, 'date', run_date=sunTime("sunset",date.today() + timedelta(days = 1)), args=[scheduler] )
+    # scheduler.add_job(openCurtains, 'date', run_date=sunTime("sunrise",date.today() + timedelta(days = 1, minutes = 30 )), args=[scheduler] )
+    scheduler.add_job(closeCurtains, 'date', run_date=datetime.now() + timedelta(minutes = 1 ), args=[scheduler] )
+    scheduler.add_job(openCurtains, 'date', run_date=datetime.now() + timedelta(minutes = 2 ), args=[scheduler] )
     logging.debug(scheduler.print_jobs())
     scheduler.start()
     logging.debug('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
