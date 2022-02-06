9c8cf73c80318b014cff79d16f1ad609c715f317
Mykola Bodnar <mykolab@nayax.com>
2022-02-06 12:53:19 +0200

    Added conn-server

--- WC_Switch_light.py
+++ WC_Switch_light.py
@@ -71,9 +71,24 @@ def changeCurtainsState(x):
             timer.start()
     logging.debug(timer.is_alive())
 
-def changeWCSwitchState(x):
-    pointLights = telnetGet("0x02030024")
-    fan = telnetGet("0x02030025")
+def fanState(x):
+    fan = telnetGet(x['fan'])
+    lightState = telnetGet(x['light'])
+    humidityState = telnetGet(x['humidity'])/100
+    logging.debug("lightState is " + str(lightState) )
+    logging.debug("humidityState is " + str(humidityState) )
+    if fan == 1:
+        if lightState == 1 or humidityState >= 50:
+            telnetSet(x['fan'],'1')
+            threading.Timer( 10.0, fanState, [x] )
+            logging.debug("Fan State of " + x['fan'] + " is 1" )
+        else:
+            telnetSet(x['fan'],'0')
+            logging.debug("Fan State of " + x['fan'] + " is 0" )
+
+def changeWCSwitchState():
+    pointLights = telnetGet("0x0102002e")
+    fan = telnetGet("0x01020025")
     backLightMirror = telnetGet("0x01020003")
     backLight = telnetGet("0x01020014")
     if pointLights == 0 and fan == 0:
@@ -85,32 +100,94 @@ def changeWCSwitchState(x):
     if backLightMirror == 0 and backLight == 0:
         telnetSet("0x01020055","1")
         telnetSet("0x01020056","0")
-        telnetSet("0x0102001D","1")
-        telnetSet("0x0102001E","0")
     else:
         telnetSet("0x01020055","0")
         telnetSet("0x01020056","1")
+    if backLightMirror == 0 and backLight == 0 and fan == 0:
+        telnetSet("0x0102001D","1")
+        telnetSet("0x0102001E","0")
+    else:
         telnetSet("0x0102001D","0")
         telnetSet("0x0102001E","1")
 def manageWCRoom(x):
-    if x[2] == '0x01010047':
-        pointLights = telnetGet("0x02030024")
-        fan = telnetGet("0x02030025")
+# WC_Switch_Up1 GSB3-40_Up1_026A2E 0x01010047 0x00000000
+# WC_Switch_Down1 GSB3-40_Down1_026A2E 0x01010048 0x00000000
+# WC_Switch_Up2 GSB3-40_Up2_026A2E 0x01010049 0x00000000
+# WC_Switch_Down2 GSB3-40_Down2_026A2E 0x0101004A 0x00000000
+# WC_Switch_Inside_Up WSB3-20-Hum_Up_02594C 0x01010019 0x00000000
+# WC_Switch_Inside_Down WSB3-20-Hum_Down_02594C 0x0101001A 0x00000000
+    logging.debug("managedWCSwitch ")
+    print(x)
+    pointLights = telnetGet("0x0102002e")
+    fan = telnetGet("0x01020025")
+    backLights = telnetGet("0x01020014")
+    mirrorLights = telnetGet("0x01020003")
+    fanSwitch = ['0x01010048', '0x0101001a']
+    lightSwitch = ['0x01010049', '0x01010019']
+    # Up1
+    if int(x[1]) == 29 and x[2] == '0x01010047' and int(x[3][:-5]) == 1:
         if pointLights == 0:
-            telnetSet("0x02030024","1")
+            telnetSet("0x0102002e","1")
         else:
-            telnetSet("0x02030024","0")
+            telnetSet("0x0102002e","0")
+        if fan == 0 and pointLights == 0:
+            telnetSet('0x01020025','1')
+    # Down1 Down1 inside
+    if int(x[1]) == 29 and x[2] in fanSwitch and int(x[3][:-5]) == 1:
         if fan == 0:
-            telnetSet("0x02030025","1")
-            
-    if x[2] == '0x01010048':
-
-    changeWCSwitchState(x)
-
+            telnetSet('0x01020025','1')
+        else:
+            telnetSet('0x01020025','0')
+    # Up2 Up1 inside
+    if int(x[1]) == 29 and x[2] in lightSwitch and int(x[3][:-5]) == 1:
+        if backLights == 0:
+            telnetSet("0x01020014","1")
+        else:
+            telnetSet("0x01020014","0")
+        if mirrorLights == 0:
+            telnetSet("0x01020003","1")
+        else:
+            telnetSet("0x01020003","0")
+    # Down2
+    if int(x[1]) == 29 and x[2] == '0x0101004a' and int(x[3][:-5]) == 1:
+        telnetSet("0x01020003","0")
+        telnetSet("0x01020014","0")
+        telnetSet("0x0102002e","0")
+        telnetSet('0x01020025','0')
 
 if __name__ == '__main__':
     try:
         tn = telnetConnect()
+        WCSwitchState = [
+            '0x0102002e',
+            '0x01020025',
+            '0x01020003',
+            '0x01020014',
+        ]
+        WCSwitch = [
+            '0x01010047',
+            '0x01010048',
+            '0x01010049',
+            '0x0101004a',
+            '0x01010019',
+            '0x0101001a',
+        ]
+        CurtainsState = [
+            '0x01020072',
+            '0x01020073',
+            '0x01020074',
+            '0x01020075',
+            '0x0102006e',
+            '0x0102006f',
+            '0x01020070',
+            '0x01020071',
+            '0x01020076',
+            '0x01020077',
+            '0x01020078',
+            '0x01020079',
+            '0x01020026',
+            '0x0102002f',
+        ]
         while True:
             try:
                 line = tn.read_until(b"\n")
@@ -125,26 +202,19 @@ if __name__ == '__main__':
             if 'EVENT' in splitted_line[0]:
                 logging.debug(line)
                 try:
-                    switchCase = {
-                        '0x02030024': changeWCSwitchState,
-                        '0x02030025': changeWCSwitchState,
-                        '0x01020003': changeWCSwitchState,
-                        '0x01020014': changeWCSwitchState,
-                        '0x01020072': changeCurtainsState,
-                        '0x01020073': changeCurtainsState,
-                        '0x01020074': changeCurtainsState,
-                        '0x01020075': changeCurtainsState,
-                        '0x0102006e': changeCurtainsState,
-                        '0x0102006f': changeCurtainsState,
-                        '0x01020070': changeCurtainsState,
-                        '0x01020071': changeCurtainsState,
-                        '0x01020076': changeCurtainsState,
-                        '0x01020077': changeCurtainsState,
-                        '0x01020078': changeCurtainsState,
-                        '0x01020079': changeCurtainsState,
-                        '0x01020026': changeCurtainsState,
-                        '0x0102002f': changeCurtainsState,
-                    }[splitted_line[2]](splitted_line)
+                    if splitted_line[2] in WCSwitch:
+                        manageWCRoom(splitted_line)
+                    if splitted_line[2] in WCSwitchState:
+                        changeWCSwitchState()
+                    if splitted_line[2] in CurtainsState:
+                        changeCurtainsState(splitted_line)
+                    if splitted_line[2] == '0x01020025':
+                        listofX = {
+                            'fan':'0x01020025',
+                            'light':'0x0102002e',
+                            'humidity':'0x01050009',
+                        }
+                        fanState(listofX)
                 except KeyError:
                     logging.debug("Key " + splitted_line[2] + " not in specific range")
     except (KeyboardInterrupt, SystemExit):
